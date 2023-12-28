d7bef0e76d4f3e906836ef2797c903419f66019a
Byte Sultan <abiolamalik30@gmail.com>
2023-12-28 09:49:53 +0100

    updated cors config

--- server/index.js
+++ server/index.js
@@ -7,12 +7,19 @@ app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
+const corsOptions = {
+  origin: [
+    'http://example.com',
+    'http://localhost:3000',
+    'https://echo-translate.vercel.app',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // enable set cookie
+  optionsSuccessStatus: 204,
+};
+
 app.use(express.json());
-app.use(
-  cors({
-    origin: '*',
-  })
-);
+app.use(cors(corsOptions));
 app.use('/', (req, res) => {
   res.send('listening');
 });
