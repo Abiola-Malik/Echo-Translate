6d0003684b17271f581458d20ca809563cb9ed12
Byte Sultan <abiolamalik30@gmail.com>
2023-12-28 09:58:32 +0100

    updated cors config

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
+app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use('/', (req, res) => {
