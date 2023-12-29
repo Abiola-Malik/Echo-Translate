dd792a2d844ca6471f4731cc80c14094ba2fae29
Byte Sultan <abiolamalik30@gmail.com>
2023-12-29 18:33:51 +0100

    updated meta tags for better seo

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -57,70 +57,73 @@ const Home = () => {
   };
 
   return (
-    <div className='w-screen h-screen bg-primary flex justify-center items-center'>
-      <form className='text-black p-4 w-full max-w-[600px]'>
-        <section className='flex flex-col gap-3 mb-3'>
-          <div className='flex flex-col md:flex-row'>
-            <label htmlFor='from-language'>From:</label>
-            <select
-              className='px-4 py-2 rounded w-[50%] mx-2 bg-black text-white'
-              id='from-language'
-              required
-              onChange={(e) => handleFromChange(e.target.value)}
-            >
-              <option value='default'></option>
+    <main className=' bg-primary w-screen h-screen '>
+      <h1 className=' text-3xl p-1 text-center'>Echo translate</h1>
+      <div className='w-full h-full flex justify-center items-center'>
+        <form className='text-black p-4 w-full max-w-[600px]'>
+          <section className='flex flex-col gap-3 mb-3'>
+            <div className='flex flex-col md:flex-row'>
+              <label htmlFor='from-language'>From:</label>
+              <select
+                className='px-4 py-2 rounded w-[50%] mx-2 bg-black text-white'
+                id='from-language'
+                required
+                onChange={(e) => handleFromChange(e.target.value)}
+              >
+                <option value='default'></option>
 
-              {options.map((option) => (
-                <option
-                  key={option.name}
-                  value={option.name}
-                  className='text-white'
-                >
-                  {option.name}
-                </option>
-              ))}
-            </select>
-            <label htmlFor='to-language'>To:</label>
-            <select
-              className='px-4 py-2 rounded w-[50%] mx-2 bg-black text-white mt-2 md:mt-0'
-              id='to-language'
-              required
-              onChange={(e) => handleToChange(e.target.value)}
-            >
-              <option value='default'></option>
-              {options.map((option) => (
-                <option
-                  key={option.name}
-                  value={option.name}
-                  className='text-white'
-                >
-                  {option.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className='flex flex-row gap-3'>
-            <input
-              type='text'
-              placeholder='Enter text'
-              className='px-4 py-2 rounded w-[75%]'
-              required
-              value={inputVal}
-              onChange={(e) => setInputVal(e.target.value)}
-            />
-            <button
-              onClick={(e) => handleTranslate(e)}
-              className='bg-secondary px-4 py-4 w-auto rounded text-black mt-2 hover:bg-opacity-80 transition duration-150 ease-in'
-            >
-              <VscSend />
-            </button>
-          </div>
-        </section>
-        <section className='my-3 h-[200px] bg-white text-black'>
-          {error ? <p className='text-red-500'>{error}</p> : content}
-        </section>
-      </form>
-    </div>
+                {options.map((option) => (
+                  <option
+                    key={option.name}
+                    value={option.name}
+                    className='text-white'
+                  >
+                    {option.name}
+                  </option>
+                ))}
+              </select>
+              <label htmlFor='to-language'>To:</label>
+              <select
+                className='px-4 py-2 rounded w-[50%] mx-2 bg-black text-white mt-2 md:mt-0'
+                id='to-language'
+                required
+                onChange={(e) => handleToChange(e.target.value)}
+              >
+                <option value='default'></option>
+                {options.map((option) => (
+                  <option
+                    key={option.name}
+                    value={option.name}
+                    className='text-white'
+                  >
+                    {option.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className='flex flex-row gap-3'>
+              <input
+                type='text'
+                placeholder='Enter text'
+                className='px-4 py-2 rounded w-[75%]'
+                required
+                value={inputVal}
+                onChange={(e) => setInputVal(e.target.value)}
+              />
+              <button
+                onClick={(e) => handleTranslate(e)}
+                className='bg-secondary px-4 py-4 w-auto rounded text-black mt-2 hover:bg-opacity-80 transition duration-150 ease-in'
+              >
+                <VscSend />
+              </button>
+            </div>
+          </section>
+          <section className='my-3 h-[200px] bg-white text-black'>
+            {error ? <p className='text-red-500'>{error}</p> : content}
+          </section>
+        </form>
+      </div>
+    </main>
   );
 };
 
